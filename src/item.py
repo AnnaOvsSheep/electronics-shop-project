f228d47d0c373114e675795f550aed650893bfe5
AnnaOvsSheep <x180475@yandex.ru>
2024-01-12 14:51:04 +0300

    code revision

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +17,18 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        Item.all.append(self)
+        self.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value[0:10]
 
     def calculate_total_price(self) -> float:
         """
@@ -34,3 +46,25 @@ class Item:
         self.price = self.price * Item.pay_rate
         return self.price
 
+    @classmethod
+    def instantiate_from_csv(cls, f_path):
+        path = "../src/items.csv"
+        f_path = os.path.abspath(path)
+        cls.all.clear()
+        with open(f_path, 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                name = row['name']
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(s: str) -> int:
+        """
+        Преобразует строку в число, если это возможно.
+
+        :param s: Строка.
+        :return: Число.
+        """
+        return int(float(s))
